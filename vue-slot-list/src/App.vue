c67d64bb05810cfcfe51965357a4b017615587a3
Tim Rayner <tim.rayner2020@gmail.com>
2024-02-03 17:12:48 +0000

    feat: added search scroll functionality into basic slot list

--- vue-slot-list/src/App.vue
+++ vue-slot-list/src/App.vue
@@ -1,11 +1,44 @@
 <script setup lang="ts">
 import FancyList from "@/components/FancyList.vue";
+import { ref, watch, type Ref } from "vue";
+import DummyData from "@/assets/data.json";
+
+const dummyData: any = ref();
+
+const searchTerm: Ref<number | null> = ref(null);
+const activeIndex: Ref<number | null> = ref(null);
+
+setTimeout(() => {
+  dummyData.value = DummyData;
+}, 1250);
+
+watch(searchTerm, () => {
+  activeIndex.value = null;
+});
+
+const findItem = () => {
+  if (searchTerm.value) {
+    //scroll to the item
+    //scroll to above the item
+    const tempScroll = searchTerm.value - 1;
+    const item = document.getElementById(`item-${tempScroll}`);
+
+    if (item) {
+      activeIndex.value = searchTerm.value ? searchTerm.value - 1 : null;
+      item.scrollIntoView({ behavior: "smooth" });
+    }
+  }
+};
 </script>
 
 <template>
   <div class="">
-    <FancyList>
+    <input type="number" v-model="searchTerm" />
+    <button class="btn" @click="findItem">Find</button>
+
+    <FancyList class="item" :activeIndex="activeIndex">
       <template #item="{ body, username, likes }">
+        <!-- content here-->
         <div class="item">
           <p>{{ body }}</p>
           <p class="meta">by {{ username }} | {{ likes }} likes</p>
--- vue-slot-list/src/components/FancyList.vue
+++ vue-slot-list/src/components/FancyList.vue
@@ -8,25 +8,40 @@ const dummyData: any = ref();
 setTimeout(() => {
   dummyData.value = DummyData;
 }, 1250);
+
+type Props = {
+  activeIndex: Number | null;
+};
+
+const props = defineProps<Props>();
 </script>
 
 <template>
-  <ul style="margin-top: 1rem">
-    <li v-if="!dummyData">Loading...</li>
-    <li v-for="item in dummyData" class="item" :key="item.id">
-      <slot name="item" v-bind="item" />
+  <ul style="margin-top: 1rem" v-if="dummyData">
+    <li
+      v-for="(item, index) in dummyData"
+      :key="item.id"
+      :id="`item-${index}`"
+      :class="{ active: activeIndex === index }"
+    >
+      <slot name="item" v-bind="item" :id="`item-${index}`" />
     </li>
   </ul>
+  <div v-else>Loading...</div>
 </template>
 
 <style scoped>
 ul {
   list-style-type: none;
 }
-ul .item {
+ul li {
   padding: 5px 20px;
   margin: 10px;
   background: #fff;
   border: solid 1px #ddd;
+}
+
+.active {
+  background: #f09865;
 }
 </style>
